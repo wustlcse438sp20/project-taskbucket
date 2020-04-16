a9d9ce7c75d948f1a665fac9d90a89d79d6db159
KBell99 <31802760+KBell99@users.noreply.github.com>
2020-04-16 16:17:42 -0400

    working on editing events

--- app/src/main/java/com/example/taskbucket/fragments/DayFragment.kt
+++ app/src/main/java/com/example/taskbucket/fragments/DayFragment.kt
@@ -1,5 +1,7 @@
 package com.example.taskbucket.fragments
 
+import android.annotation.SuppressLint
+import android.nfc.Tag
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
@@ -22,6 +24,7 @@ import java.text.SimpleDateFormat
 import java.util.*
 import kotlin.collections.ArrayList
 import androidx.lifecycle.Observer
+import java.time.Year
 
 /**
  * A simple [Fragment] subclass.
@@ -34,88 +37,76 @@ private var cal: Calendar = Calendar.getInstance()
 class DayFragment() : Fragment() {
     val TAG: String = "DayFragment"
     val test = true
+
+    lateinit var viewModel:EventViewModel
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_day, container, false)
     }
 
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        viewModel = ViewModelProvider(this).get(EventViewModel::class.java)
+        viewModel.getEventsByYear(2020)
+        viewModel.currentEvents.observe(viewLifecycleOwner, Observer {
+            Log.d(TAG, "Events: " + it)
+        })
 
 
         initDay(view)
-
-
+        populateDayTable()
         day_prev_button.setOnClickListener {
             prevDay(view)
+            populateDayTable()
         }
 
         day_next_button.setOnClickListener {
             nextDay(view)
+
         }
+
+
+
+
+    }
+
+
+    fun populateDayTable() {
+        var currentEvents: ArrayList<Event> = ArrayList()
+        viewModel.getEventsByDay(cal.get(Calendar.YEAR), cal.get(Calendar.MONTH), cal.get(Calendar.DAY_OF_MONTH))
+
+        viewModel.currentEvents.observe(viewLifecycleOwner, Observer {
+            currentEvents = ArrayList(it)
+        })
+
 
         recyclerView_day.layoutManager = LinearLayoutManager(requireContext())
         var items = arrayListOf<daygridEvent>()
         var emptyEvents = arrayListOf<Event>()
 
-        var event1 = Event("test", cal.get(Calendar.MONTH), cal.get(Calendar.DAY_OF_MONTH), 2020, 300, 600, "blahblah blash",cal.get(Calendar.WEEK_OF_MONTH),cal.get(Calendar.WEEK_OF_YEAR), 0)
-        var event2 = Event("test", 3, 4, 2020, 5, 7, "blahblah blash",3,13, 0)
-        var event3 = Event("test", 3, 4, 2020, 5, 9, "blahblah blash",3,13, 0)
-        var event4 = Event("test", 3, 4, 2020, 5, 6, "blahblah blash",3,13, 0)
-        var events = arrayListOf<Event>()
-        events.add(event1)
-        events.add(event2)
-        events.add(event3)
-        events.add(event4)
-        Log.d(TAG, "onViewCreated: " + events.size)
-        for(i in 0..23){
-            if(i == 10){
-                items.add((daygridEvent(i, events)))
-            }else{
-                items.add(daygridEvent(i, emptyEvents))
-            }
 
+
+        for(i in 0..23){
+            var dayEvents = arrayListOf<Event>()
+            for(event in currentEvents){
+                if(i ==10 ){
+                    items.add((daygridEvent(i, dayEvents)))
+                    Log.d(TAG, "onViewCreated: " + dayEvents.size)
+                }else{
+                    items.add(daygridEvent(i, emptyEvents))
+                }
+
+            }
         }
         recyclerView_day.adapter = daygridAdapter(items,requireActivity())
         recyclerView_day.addItemDecoration(DividerItemDecoration(activity, DividerItemDecoration.VERTICAL))
 
-
-            var viewModel = ViewModelProvider(this).get(EventViewModel::class.java)
-
-
-    }
-
-    override fun onResume() {
-        super.onResume()
-
-        populateDayTable()
-    }
-
-    fun populateDayTable() {
-        //val chart = activity!!.findViewById<RelativeLayout>(R.id.day_table)
-        //val chart_height = chart.height
-        //val chart_width = chart.width
-
-//        println("Height: " + chart_height)
-//        println("Width: " + chart_width)
-//
-//        for(event in eventList){
-//            val btn = Button(context)
-//            val btn_double:Double = ((event.end_time - event.start_time)/60.0) * (chart_height/24.0)
-//            val btn_height:Int = btn_double.toInt()
-//            val btn_width:Int = chart_width-(chart_width* 16/100)
-//            println("Height: " + btn_height)
-//            println("Width: " + btn_width)
-//            btn.text = event.title
-//            btn.layoutParams = LinearLayout.LayoutParams(btn_width,btn_height)
-//            btn.x = chart_width*0.16F
-//            btn.y = event.start_time/60F * (chart_height/24)
-//            //chart.addView(btn)
-//        }
     }
 
     fun initDay(view: View) {
